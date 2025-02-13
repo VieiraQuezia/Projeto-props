36c678762b5bf246db31187624be091c628bc504
Quezia Vieira <vieiraquezia24@gmail.com>
2025-02-13 09:02:04 -0300

    att 8

--- Desafio_Props/src/App.jsx
+++ Desafio_Props/src/App.jsx
@@ -1,27 +1,13 @@
-<<<<<<< HEAD
-// import guilherme from "./assets/guilherme.png"
-// import hayeska from "./assets/hayeska.png"
-// import leonardo from "./assets/leonardo.png"
-// import quezia from "./assets/quezia.png"
-import gmail from '../src/assets/gmail.png'
-import instagram from '../src/assets/instagram.png'
-=======
 import guilherme from "./assets/guilherme.png"
 import hayeska from "./assets/hayeska.png"
 import leonardo from "./assets/leonardo.png"
 import quezia from "./assets/quezia.png"
 import instagram from "./assests/instagram.png"
->>>>>>> 0022b293e831a98fe82337d7b08c15195a305a0d
 import logo from "./assets/logo.png";
 import Usuarios from "./components/Usuarios.jsx";
 import "./App.css";
 import Lado from './components/lado'
-<<<<<<< HEAD
-import Footer from './components/Footer.jsx';
-
-=======
 import Cabecalho from "./components/header.jsx";
->>>>>>> 0022b293e831a98fe82337d7b08c15195a305a0d
 
 
 function App() {
@@ -36,20 +22,7 @@ function App() {
         <Usuarios nome="Leonardo M. Vicente" img={leonardo} hobby="Tocar piano"/>
         <Usuarios nome="Quezia A. Vieira" img={quezia} hobby="Jogar vôlei"/>
         <Lado/>
-<<<<<<< HEAD
-<<<<<<< HEAD
-      </div>
-
-
-      <Footer imge={instagram} texto="@vieiraquezia08" />
-      <Footer imge={gmail} texto="vieiraquezia24@gmail.com"/>
-      <Footer texto="Responsável pelo desenvolvimento: Quezia Amaral Vieira"/>
-=======
-        </article>
->>>>>>> 7a0d5d57b7113c7e750d831937fb10b85db4a4b5
-=======
       </article>
->>>>>>> 0022b293e831a98fe82337d7b08c15195a305a0d
     </>
   );
 }
