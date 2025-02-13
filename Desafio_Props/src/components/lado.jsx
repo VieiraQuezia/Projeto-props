5ebf4b6e327f3f4ffe23c9d307aac08eae365f2d
Leonardo <leomori3107@gmail.com>
2025-02-13 08:21:36 -0300

    lado e lado.css atualizados

--- Desafio_Props/src/components/lado.jsx
+++ Desafio_Props/src/components/lado.jsx
@@ -3,12 +3,14 @@ import "./lado.css"
 function Lado(props) {
   return (
     <>
+    <div className="aside">
         <h2>Valores do grupo</h2>
         <ul>
           <li>{props.val1}</li>
           <li>{props.val2}</li>
           <li>{props.val3}</li>
         </ul>
+    </div>
     </>
   );
 }
