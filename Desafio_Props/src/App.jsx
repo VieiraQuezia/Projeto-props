98c00e12e546f5bda8a31e102911ca6a543e46a7
Leonardo <leomori3107@gmail.com>
2025-02-13 10:37:45 -0300

    aside do lado

--- Desafio_Props/src/App.jsx
+++ Desafio_Props/src/App.jsx
@@ -12,17 +12,19 @@ import Cabecalho from "./components/header.jsx";
 function App() {
   return (
     <>
-    
+
     <header>
       <Cabecalho/>
     </header>
+    <div className="divmeio">
     <article className="blocos">
         <Usuarios nome="Guilherme G. Santana" img={guilherme} hobby="Sair com a namorada"/>
         <Usuarios nome="Hayeska L. A. Machado" img={hayeska} hobby="Jogar vôlei"/>
         <Usuarios nome="Leonardo M. Vicente" img={leonardo} hobby="Tocar piano"/>
         <Usuarios nome="Quezia A. Vieira" img={quezia} hobby="Jogar vôlei"/>
-        <Lado/>
       </article>
+      <Lado/>
+      </div>
     </>
   );
 }
