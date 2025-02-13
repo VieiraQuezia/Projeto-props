fd618f851e69b2c1728ad86edf2d698f21238a61
Quezia Vieira <vieiraquezia24@gmail.com>
2025-02-13 08:54:00 -0300

    att 6

--- Desafio_Props/src/App.jsx
+++ Desafio_Props/src/App.jsx
@@ -2,7 +2,7 @@ import guilherme from "./assets/guilherme.png"
 import hayeska from "./assets/hayeska.png"
 import leonardo from "./assets/leonardo.png"
 import quezia from "./assets/quezia.png"
-// import instagram from "./assests/instagram.png"
+import instagram from "./assests/instagram.png"
 import logo from "./assets/logo.png";
 import Usuarios from "./components/Usuarios.jsx";
 import "./App.css";
@@ -13,7 +13,6 @@ import Lado from './components/lado'
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <div className="cabeca">
         <img src={logo} alt="Logo do Grupo" />
         <h1>DESAFIO 1 de PROPS - REACT</h1>
@@ -23,12 +22,8 @@ function App() {
         <Usuarios nome="Hayeska L. A. Machado" img={hayeska} hobby="Jogar vôlei"/>
         <Usuarios nome="Leonardo M. Vicente" img={leonardo} hobby="Tocar piano"/>
         <Usuarios nome="Quezia A. Vieira" img={quezia} hobby="Jogar vôlei"/>
-=======
-    <article className="Blocao">
-        <Usuarios />
->>>>>>> 7a0d5d57b7113c7e750d831937fb10b85db4a4b5
         <Lado/>
-        </article>
+      </div>
     </>
   );
 }
