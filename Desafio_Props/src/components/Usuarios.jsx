5a948513036e39039de9c22d233a3e1f79f19e3d
Guih <ggsantana18@gmail.com>
2025-02-13 07:38:43 -0300

    att1

---
+++ Desafio_Props/src/components/Usuarios.jsx
@@ -0,0 +1,52 @@
+import './Usuarios.css'
+import './App.css'
+
+function perfil() {
+
+  return (
+    <>
+      <article className="blocos">
+
+<nav className="bloco">
+
+<h2> Guilherme Gariel </h2>
+<img className="imagem" src="" />
+<h3> Hobby:  </h3>
+<p> Sair com namorada </p>
+
+</nav>
+
+
+<nav className="bloco">
+
+<h2> Hayeska Loredane </h2>
+<img className="imagem" src="" />
+<h3> Hobby:  </h3>
+<p> Jogar vôlei </p>
+
+</nav>
+
+
+<nav className="bloco">
+
+<h2> Leonardo Mori </h2>
+<img className="imagem" src="" />
+<h3> Hobby:  </h3>
+<p> Toacar piano</p>
+
+</nav>
+<nav className="bloco">
+
+<h2> Quezia </h2>
+<img className="imagem" src="" />
+<h3> Hobby:  </h3>
+<p> Jogar vôlei </p>
+
+</nav>
+
+</article>
+    </>
+  )
+}
+
+export default perfil
