f86a57125cad89d59395d50e994919f65ef05cc2
Guih <ggsantana18@gmail.com>
2025-02-13 08:12:10 -0300

    att1

--- Desafio_Props/src/App.jsx
+++ Desafio_Props/src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
 import Lado from './components/lado'
+
+import Usuarios from './components/Usuarios'
+
 
 function App() {
   const [count, setCount] = useState(0)
